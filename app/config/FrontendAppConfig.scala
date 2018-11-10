9be2679e15348fd1d769bb0786e4e9fd63b2bd6c
daniel-manning <daniel.manning@digital.hmrc.gov.uk>
2018-11-10 17:38:22 +0000

    WIP for play 2.6 update

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -16,34 +16,30 @@
 
 package config
 
-import com.google.inject.{Inject, Singleton}
-import play.api.{Configuration, Environment}
-import play.api.i18n.Lang
+import play.api.Configuration
 import controllers.routes
-import uk.gov.hmrc.play.config.ServicesConfig
+import javax.inject.Inject
+import play.api.i18n.Lang
+
 
 @Singleton
-class FrontendAppConfig @Inject() (override val runModeConfiguration: Configuration, environment: Environment) extends ServicesConfig {
+class FrontendAppConfig @Inject()(configuration: Configuration){
 
-  override protected def mode = environment.mode
-
-  private def loadConfig(key: String) = runModeConfiguration.getString(key).getOrElse(throw new Exception(s"Missing configuration key: $key"))
-
-  private lazy val contactHost = runModeConfiguration.getString("contact-frontend.host").getOrElse("")
+  private val contactHost = configuration.get[String]("contact-frontend.host")
   private val contactFormServiceIdentifier = "play26frontend"
 
-  lazy val analyticsToken = loadConfig(s"google-analytics.token")
-  lazy val analyticsHost = loadConfig(s"google-analytics.host")
-  lazy val reportAProblemPartialUrl = s"$contactHost/contact/problem_reports_ajax?service=$contactFormServiceIdentifier"
-  lazy val reportAProblemNonJSUrl = s"$contactHost/contact/problem_reports_nonjs?service=$contactFormServiceIdentifier"
-  lazy val betaFeedbackUrl = s"$contactHost/contact/beta-feedback"
-  lazy val betaFeedbackUnauthenticatedUrl = s"$contactHost/contact/beta-feedback-unauthenticated"
+  val analyticsToken = configuration.get[String](s"google-analytics.token")
+  val analyticsHost = configuration.get[String](s"google-analytics.host")
+  val reportAProblemPartialUrl = s"$contactHost/contact/problem_reports_ajax?service=$contactFormServiceIdentifier"
+  val reportAProblemNonJSUrl = s"$contactHost/contact/problem_reports_nonjs?service=$contactFormServiceIdentifier"
+  val betaFeedbackUrl = s"$contactHost/contact/beta-feedback"
+  val betaFeedbackUnauthenticatedUrl = s"$contactHost/contact/beta-feedback-unauthenticated"
 
-  lazy val authUrl = baseUrl("auth")
-  lazy val loginUrl = loadConfig("urls.login")
-  lazy val loginContinueUrl = loadConfig("urls.loginContinue")
+  val authUrl = configuration.get[Service]("auth")
+  val loginUrl = configuration.get[String]("urls.login")
+  val loginContinueUrl = configuration.get[String]("urls.loginContinue")
 
-  lazy val languageTranslationEnabled = runModeConfiguration.getBoolean("microservice.services.features.welsh-translation").getOrElse(true)
+  val languageTranslationEnabled = configuration.get[Boolean]("microservice.services.features.welsh-translation")
   def languageMap: Map[String, Lang] = Map(
     "english" -> Lang("en"),
     "cymraeg" -> Lang("cy"))
--- app/controllers/actions/IdentifierAction.scala
+++ app/controllers/actions/IdentifierAction.scala
@@ -17,7 +17,7 @@
 package controllers.actions
 
 import com.google.inject.Inject
-import play.api.mvc.{ActionBuilder, ActionFunction, Request, Result}
+import play.api.mvc._
 import play.api.mvc.Results._
 import uk.gov.hmrc.auth.core._
 import uk.gov.hmrc.auth.core.retrieve.Retrievals
@@ -62,10 +62,11 @@ class AuthenticatedIdentifierAction @Inject()(override val authConnector: AuthCo
   }
 }
 
-trait IdentifierAction extends ActionBuilder[IdentifierRequest] with ActionFunction[Request, IdentifierRequest]
+trait IdentifierAction extends ActionBuilder[IdentifierRequest, AnyContent] with ActionFunction[Request, IdentifierRequest]
 
-class SessionIdentifierAction @Inject()(config: FrontendAppConfig)
-                                 (implicit ec: ExecutionContext) extends IdentifierAction {
+class SessionIdentifierAction @Inject()(config: FrontendAppConfig,
+                                        val parser: BodyParsers.Default)
+                                 (implicit val executionContext: ExecutionContext) extends IdentifierAction {
 
   override def invokeBlock[A](request: Request[A], block: (IdentifierRequest[A]) => Future[Result]): Future[Result] = {
     implicit val hc: HeaderCarrier = HeaderCarrierConverter.fromHeadersAndSession(request.headers, Some(request.session))
